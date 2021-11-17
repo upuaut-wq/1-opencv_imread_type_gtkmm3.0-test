74543104ddea0de60f4619f5c38b0b826a261ea9
Leonardo de Jesus Queiroz <upuaut@tutanota.com>
2021-11-16 22:47:32 -0300

    Melhora na Documentaçao

--- imread.cpp
+++ imread.cpp
@@ -3,8 +3,10 @@
 
 
 ImRead::ImRead(){
+    //Titulo da janela
     this->set_title("Exemplo IMREAD OPENCV");
 
+    //Caixa principal
     this->add(this->m_main_box);
     this->m_main_box.set_orientation(Gtk::ORIENTATION_HORIZONTAL);
     this->m_main_box.pack_start(this->m_dir_box);
@@ -13,6 +15,20 @@ ImRead::ImRead(){
     this->m_dir_box.pack_start(this->m_dir_box2);
     this->m_main_box.pack_start(this->m_esq_box);
     this->m_esq_box.set_orientation(Gtk::ORIENTATION_VERTICAL);
+
+    //Permite que a box se posicione na parte superior deixando todo o conteudo na parte 
+    //superior dentro dos limites de cada box, frame e botôes
+    //////////////////////////////////////////////////////////////////
+    //----------------------------------
+    //|                     |           |  
+    //|                     |-----------|
+    //|                     |           |  <-  Alinha conteudo na parte superior FORÇANDO
+    //|                     |           |      o conteúdo se ajustar na parte superior.
+    //|                     |           |
+    //|                     |-----------|
+    //|                                 |
+    //|                                 |
+    //----------------------------------
     this->m_esq_box.set_valign(Gtk::ALIGN_START);
 
     Gtk::RadioButtonGroup grupo;
@@ -20,9 +36,11 @@ ImRead::ImRead(){
     this->m_esq_box.pack_start(frame_entry);
     this->frame_entry.add(this->m_frame_box);
     
+    //Seta Label ao Gtk::Frama que segura o Gtk::Entry
     this->carregar.set_label("Carregar Image");
     this->m_frame_box.pack_start(this->m_entry);
-    this->frame_entry.set_valign(Gtk::ALIGN_START);
+
+    //Seta margens (Visual)
     this->m_frame_box.set_margin_top(5);
     this->m_frame_box.set_margin_bottom(10);
     this->m_frame_box.set_margin_start(10);
@@ -30,23 +48,36 @@ ImRead::ImRead(){
     this->frame_entry.set_label(" Imagem ");
     this->frame_entry.set_label_align(0.05);
     this->m_frame_box.pack_start(this->carregar);
-    this->m_entry.set_valign(Gtk::ALIGN_BASELINE);
 
+    //Sinal para carregar uma umagem.
     this->carregar.signal_clicked().connect(sigc::mem_fun(*this,&ImRead::on_btn_carregar));
     
+    //Configuração das opções de carregamento. 
+    //Adiciona um label no Gtk::Frame
     this->frame_radio.set_label(" Tipo de Carregamento ");
+
+    //Cria espaços para que o Label não fique soporeposto na borda esquerda e superior.
     this->frame_radio.set_margin_top(10);
     this->frame_radio.set_label_align(0.05);
+
+    //Adiciona o segundo grama em uma box.
     this->m_esq_box.pack_start(this->frame_radio);
     this->frame_radio.add(this->m_esq_box1);
+
+    //Seta uma orientação para box que vai segurar os radio buttons.
+    //Seta margins na box.
     this->m_esq_box1.set_orientation(Gtk::ORIENTATION_VERTICAL);
     this->m_esq_box1.set_margin_top(5);
     this->m_esq_box1.set_margin_bottom(10);
     this->m_esq_box1.set_margin_start(10);
     this->m_esq_box1.set_margin_end(10);
 
+    //Configura os dario button
+    //Adiciona o botão na box
     this->m_esq_box1.pack_start(this->btn01);
+    //Seta um label
     this->btn01.set_label("IMREAD_UNCHANGED");
+    //Adiciona o radio button no grupo chamado grupo.
     this->btn01.set_group(grupo); 
     this->m_esq_box1.pack_start(this->btn02);
     this->btn02.set_label("IMREAD_GRAYSCALE ");
@@ -84,13 +115,23 @@ ImRead::ImRead(){
     this->m_esq_box1.pack_start(this->btn13);
     this->btn13.set_label("IMREAD_IGNORE_ORIENTATION");
     this->btn13.set_group(grupo);
+
+    //Seta um default no Gtk::Entry
     this->m_entry.set_text("img_test.png");
-     this->m_dir_box1.pack_start(this->imgor);
+    
+    //Adiciana as Gtk::Imagem nas devidas box
+    this->m_dir_box1.pack_start(this->imgor);
     this->m_dir_box2.pack_start(this->imgdes);
+
+    //Adiciana na "referência" das imagens a informação dos arquivos 
     this->img_or = Gdk::Pixbuf::create_from_file("or.png");
     this->img_des = Gdk::Pixbuf::create_from_file("ds.png");
+
+    //Adiciona as referencias nas Gtk::Imagens
     imgdes.set(this->img_des);
     imgor.set(this->img_or);
+
+    //Exibe todos os sub widges adicionadas na Gtk::Window.
     this->show_all_children();
 
 }
